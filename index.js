0678ee570d2c87c9d88b82bf35bc9bddf07ac93f
luqingxuan <luqingxuan354@126.com>
2017-03-06 14:38:47 +0800

    update
    init

--- index.js
+++ index.js
@@ -10,27 +10,25 @@
         // 浏览器全局变量(root 即 window)
         root.returnExports = factory();
     }
-}(this, function($) {
-    return function obtainWindowRect() {
-        var width = window.innerWidth;
-        var height = window.innerHeight;
+}(this, function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
-        if (typeof width != 'number') { // IE 5/6/7/8
-            if (document.compatMode == 'CSS1Compat') {
-                width = document.documentElement.clientWidth;
-                height = document.docuementElement.clientHeight;
-            } else {
-                width = document.body.clientWidth;
-                height = document.body.clientHeight;
-            }
+    if (typeof width != 'number') { // IE 5/6/7/8
+        if (document.compatMode == 'CSS1Compat') {
+            width = document.documentElement.clientWidth;
+            height = document.docuementElement.clientHeight;
+        } else {
+            width = document.body.clientWidth;
+            height = document.body.clientHeight;
         }
-
-        return {
-            width: width,
-            height: height,
-            top: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
-            left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
-        };
     }
+
+    return {
+        width: width,
+        height: height,
+        top: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
+        left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
+    };
 
 }));
